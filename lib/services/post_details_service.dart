fed487d91bce23671a8fce78844e5aa332558729
Abdullah Al Roman <abdullahr583@live.com>
2025-09-12 18:56:04 +0600

    add progressIndicator

---
+++ lib/services/post_details_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:technical_assignment/models/user_model.dart';
+import 'package:technical_assignment/utils/global.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:path_provider/path_provider.dart';
+
+downloadFileFromServer() async {
+  ValueNotifier downloadProgressNotifier = ValueNotifier(0);
+    Directory directory = Directory("");
+    if (Platform.isAndroid) {
+      directory = (await getExternalStorageDirectory())!;
+    } else {
+      directory = (await getApplicationDocumentsDirectory());
+    }
+    await Dio().download(
+        "https://sample-videos.com/img/Sample-jpg-image-5mb.jpg",
+        '${directory.path}/sampleImage.pdf',
+        onReceiveProgress: (actualBytes, int totalBytes) {
+      downloadProgressNotifier.value = (actualBytes / totalBytes * 100).floor();
+    });
+    print('File downloaded at ${directory.path}/samplePDF.pdf');
+  }
--- lib/views/post_details_screen.dart
+++ lib/views/post_details_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:technical_assignment/models/post_model.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:dio/dio.dart';
 
 class PostDetailScreen extends StatefulWidget {
 PostModel post;
@@ -8,8 +14,8 @@ PostModel post;
   @override
   State<PostDetailScreen> createState() => _PostDetailsScreenState();
 }
-
 class _PostDetailsScreenState extends State<PostDetailScreen> {
+  ValueNotifier downloadProgressNotifier = ValueNotifier(0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,9 +38,46 @@ class _PostDetailsScreenState extends State<PostDetailScreen> {
                       SizedBox(height: 10), 
                       ElevatedButton(
                     onPressed: () {
+                      downloadFileFromServer();
                     },
                     child:  Text('Download File'),
-                  )              
+                  ),
+                  // show progress indicators using the ValueListenableBuilder
+                  ValueListenableBuilder(
+                    valueListenable: downloadProgressNotifier,
+                    builder: (context, value, child) {
+                      final percentValue = (value is num) ? (value.toDouble() / 100.0) : 0.0;
+                      final displayText = "${value ?? 0}%";
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [                          
+                          SizedBox(
+                            height: 32,
+                          ),
+                          LinearPercentIndicator(
+                            // animation: true,
+                            barRadius: const Radius.circular(10),
+                            // animationDuration: 400,
+                            lineHeight: 15.0,
+                            percent: percentValue.clamp(0.0, 1.0),
+                            backgroundColor: Colors.grey.shade300,
+                            progressColor: Colors.blue,
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            displayText,
+                            style: const TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
                     ],
                   ),
             ],
@@ -43,4 +86,20 @@ class _PostDetailsScreenState extends State<PostDetailScreen> {
       ),
     );
   }
-}
+downloadFileFromServer() async {  
+    Directory directory = Directory("");
+    if (Platform.isAndroid) {
+      directory = (await getExternalStorageDirectory())!;
+    } else {
+      directory = (await getApplicationDocumentsDirectory());
+    }
+    await Dio().download(
+        "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+        '${directory.path}/dummy.pdf',
+        onReceiveProgress: (actualBytes, int totalBytes) {
+      downloadProgressNotifier.value = (actualBytes / totalBytes * 100).floor();
+    });
+    print('File downloaded at ${directory.path}/dummy.pdf');
+  }
+  
+} 
